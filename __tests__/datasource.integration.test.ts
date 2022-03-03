8ae548faac50c4b5497358569acd99f6af061f59
Joe <joe@duplocloud.net>
2022-03-03 12:18:48 -0500

    reorganize tests

---
+++ __tests__/datasource.integration.test.ts
@@ -0,0 +1,71 @@
+import {expect, test} from '@jest/globals'
+
+import {DataSource} from '../src/duplocloud/datasource'
+import {DuploHttpClient} from '../src/duplocloud/httpclient'
+
+describe('DataSource integration', () => {
+
+  // Integration tests.
+  const tenantId = process.env.duplo_tenant_id
+  if (!tenantId || ! process.env.duplo_token) {
+    console.log('Skipping integration tests: duplo_token and/or duplo_tenant_id env var missing or empty')
+    it('is skipped', () => { })
+  }
+
+  else {
+    const ds = new DataSource(new DuploHttpClient())
+
+    describe('getTenantsForUser', () => {
+      it('lists tenants accessible to the user', async () => {
+        const result = await ds.getTenantsForUser().toPromise()
+        expect(result).not.toBeNull()
+      })
+    })
+
+    describe('getTenantByName', () => {
+      it('converts uppercase name', async () => {
+        const result = await ds.getTenantByName('DEFAULT').toPromise()
+        expect(result).not.toBeNull()
+        expect(result?.AccountName).toBe('default')
+      })
+
+      it('can get default tenant', async () => {
+        const result = await ds.getTenantByName('default').toPromise()
+        expect(result).not.toBeNull()
+        expect(result?.AccountName).toBe('default')
+      })
+    })
+
+    describe('getTenant', () => {
+
+      it('converts uppercase name', async () => {
+        const result = await ds.getTenant('DEFAULT').toPromise()
+        expect(result).not.toBeNull()
+        expect(result?.AccountName).toBe('default')
+      })
+
+      it('can get tenant by name or ID', async () => {
+        const result = await ds.getTenantByName('default').toPromise()
+        expect(result?.TenantId).not.toBeNull()
+
+        if (result?.TenantId) {
+          const resultByName = await ds.getTenant('default').toPromise()
+          expect(resultByName).toEqual(result)
+          const resultById = await ds.getTenant(result.TenantId).toPromise()
+          expect(resultById).toEqual(result)
+        }
+      })
+    })
+
+    describe('getTenantAwsJitCredentials', () => {
+      it('can get AWS JIT creds for tenant', async () => {
+        const result = await ds.getTenantAwsJitCredentials(tenantId).toPromise()
+        expect(result).not.toBeNull()
+        expect(result.Region).not.toBeNull()
+        expect(result.AccessKeyId).not.toBeNull()
+        expect(result.SecretAccessKey).not.toBeNull()
+        expect(result.SessionToken).not.toBeNull()
+      })
+    })
+  }
+})
---
+++ __tests__/datasource.unit.test.ts
@@ -0,0 +1,25 @@
+import {beforeEach, expect, jest, test} from '@jest/globals'
+import { EMPTY } from 'rxjs'
+
+import {DataSource} from '../src/duplocloud/datasource'
+import {DuploHttpClient} from '../src/duplocloud/httpclient'
+import { AwsJitCredentials } from '../src/duplocloud/model'
+
+//jest.mock('../../src/duplocloud/httpclient')
+
+const mockDuploHttpClient_doFetch = jest
+  .spyOn(DuploHttpClient.prototype, 'doFetch' as any)
+  .mockImplementation(() => EMPTY)
+
+
+describe('DataSource unit', () => {
+  const ds = new DataSource(new DuploHttpClient())
+
+  describe('getSystemFeatures', () => {
+    it('does a GET to /v3/features/system', async () => {
+      ds.getSystemFeatures()
+      expect(mockDuploHttpClient_doFetch).
+        toHaveBeenCalledWith('/v3/features/system', 'GET', null, undefined)
+    })
+  })
+})
